cb8ae521a6c34e884165ac0ee872cb65c6041a16
jackeychen <jackeychen@tencent.com>
2019-01-16 19:37:53 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-打包脚本
-"""
-    
+
+# -*- coding:UTF-8 -*-
+#
+# Tencent is pleased to support the open source community by making QTA available.
+# Copyright (C) 2016THL A29 Limited, a Tencent company. All rights reserved.
+# Licensed under the BSD 3-Clause License (the "License"); you may not use this 
+# file except in compliance with the License. You may obtain a copy of the License at
+# 
+# https://opensource.org/licenses/BSD-3-Clause
+# 
+# Unless required by applicable law or agreed to in writing, software distributed 
+# under the License is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS
+# OF ANY KIND, either express or implied. See the License for the specific language
+# governing permissions and limitations under the License.
+#
+
 import os
 from setuptools import setup, find_packages
   
@@ -26,16 +37,51 @@ def parse_requirements():
                 if line:
                     reqs.append(line)
     return reqs
-  
+
+def list_data_files(packages):
+    '''遍历package中的文件并查找所有资源文件
+    
+    :returns: list
+    '''
+    data_files = []
+    for package in packages:
+        file_path = os.path.join(BASE_DIR, *package.split('.'))
+        if os.path.isdir(file_path):
+            for dirpath, dirnames, filenames in os.walk(file_path):
+                dirname = '/'.join(os.path.relpath(dirpath, BASE_DIR).split(os.path.sep))
+                child_data_files = []
+                for filename in filenames:
+                    if not filename.endswith('.py') and not filename.endswith('.pyc'):
+                        child_data_files.append(dirname+'/'+filename)
+                if child_data_files:
+                    data_files.append((dirname, child_data_files))
+        else:
+            dirname = '/'.join(package.split('.')[0:-1])    
+            data_files.append(dirname+'/'+package.split('.')[-1])
+    return data_files 
+
+def get_description():
+    with open(os.path.join(BASE_DIR, "README.md"), "r") as fh:
+        return fh.read()
+     
 if __name__ == "__main__":  
     setup(
+        zip_safe=False,
         version=generate_version(),
-        name="qt4w",
+        name="qt4a",
         cmdclass={},
-        packages=find_packages(),
-        package_data={'':['*.txt', '*.TXT'], },
-        data_files=[(".", ["version.txt"])],
+        packages=find_packages(exclude=("test", "test.*",)),
+        include_package_data=True,
+        description="QTA driver for Web",
+        long_description=get_description(),
+        long_description_content_type="text/markdown",
         author="Tencent",
         license="Copyright(c)2010-2018 Tencent All Rights Reserved. ",
-        install_requires=parse_requirements(),
+        install_requires=parse_requirements(),  
+        classifiers=[
+          "Programming Language :: Python :: 2.7",
+          "Operating System :: OS Independent",
+        ],
+        url="https://github.com/qtacore/QT4W",
+        project_urls={"QT4W Documentation":"https://qt4w.readthedocs.io/zh_CN/latest/",},
     )
